6c932ad87dbc71ec4a82fe1f3db49153a211259d
AMAR SHAW <46993795+shawamar@users.noreply.github.com>
2020-06-15 00:35:13 +0530

    CS891

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 Online Library Management System |<a href="https://newborn-vibration.000webhostapp.com/" target="_blank"  > Designed by : Amar Shaw</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
